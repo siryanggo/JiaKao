948ebb8560d598e68481b65af2ccfd6574a3ee0c
12栋奥特曼 <1106367655@qq.com>
2022-06-03 21:57:12 +0800

    第二 次提交

--- src/main/java/com/mj/jk/JiaKaoApplication.java
+++ src/main/java/com/mj/jk/JiaKaoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JiaKaoApplication {
     public static void main(String[] args) {
+        System.out.println("hello");
         SpringApplication.run(JiaKaoApplication.class, args);
     }
 }
